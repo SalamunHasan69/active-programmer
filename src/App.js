cdb00c98839db6717a3949b4170f70153de04d85
SalamunHasan69e <salamun2600@gmail.com>
2022-09-29 21:36:13 +0600

    added meaningful name and title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div>
       <nav>
         <div className="navbar bg-slate-50 justify-around">
-          <h1 className=" normal-case text-2xl font-extrabold text-primary italic ml-10">Assignment-8</h1>
+          <h1 className=" normal-case text-2xl font-extrabold text-primary italic ml-10">Active Programmer</h1>
           <p className='text-lg font-semibold italic'>Daily practice</p>
         </div>
       </nav>
